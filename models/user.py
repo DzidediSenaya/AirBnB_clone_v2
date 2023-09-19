bed78f0cdd70e8fc507aa93b5c4c096cc3d72cc4
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-09-19 20:33:41 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -8,7 +8,8 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """The User class, contains user information"""
 
-    __tablename__ = "users"
+    __tablename__ = "users"  # Define the table name
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
@@ -16,3 +17,6 @@ class User(BaseModel, Base):
 
     # Define the one-to-many relationship with Place
     places = relationship("Place", backref="user", cascade="all, delete-orphan")
+
+    # Define the one-to-many relationship with Review
+    reviews = relationship("Review", back_populates="user", cascade="all, delete-orphan")
