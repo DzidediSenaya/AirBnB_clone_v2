35a475f6a219737429460f6d9630dd76cfeefc4c
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-09-19 20:39:28 +0000

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 """Place Module for HBNB project"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
+from os import getenv
+
+# Create an instance of SQLAlchemy Table for the Many-To-Many relationship
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True,
+                             nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True,
+                             nullable=False)
+                      )
 
 
 class Place(BaseModel, Base):
@@ -26,15 +39,25 @@ class Place(BaseModel, Base):
     # Define the many-to-one relationship with User
     user = relationship("User", back_populates="places")
 
-    # Define the one-to-many relationship with Review for DBStorage
-    reviews = relationship("Review", backref="place", cascade="all, delete-orphan")
+    # For DBStorage: Define the many-to-many relationship with Amenity
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False)
+    else:
+        @property
+        def amenities(self):
+            """Getter attribute for amenities"""
+            from models import storage
+            amenities = []
+            for amenity_id in self.amenity_ids:
+                key = "Amenity." + amenity_id
+                amenity = storage.all("Amenity").get(key)
+                if amenity:
+                    amenities.append(amenity)
+            return amenities
 
-    @property
-    def reviews(self):
-        """Getter attribute for reviews (FileStorage)"""
-        from models import storage
-        reviews_list = []
-        for review in storage.all(Review).values():
-            if review.place_id == self.id:
-                reviews_list.append(review)
-        return reviews_list
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter attribute for amenities"""
+            if isinstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)
