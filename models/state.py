c839486fe0e4ecdcb01de88c88a0411aece8f736
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-09-21 04:48:50 +0000

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"  # Define the table name
 
     name = Column(String(128), nullable=False)
 
     # Establish a relationship with the City class
-    cities = relationship("City", back_populates="state")
+    cities = relationship("City", backref="state", cascade="delete")
