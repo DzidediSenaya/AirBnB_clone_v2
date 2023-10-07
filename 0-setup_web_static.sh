e653dc567829968b8b8b9a3ccfd83b378a0114c6
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-10-07 10:35:35 +0000

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,50 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Initialize the "msg" variable with the expected value
+msg="0"
 
-apt-get update
-apt-get -y install nginx
-# Install Nginx if it not already installed
-mkdir -p /data/web_static/releases/test/
-# Create the folder /data/ if it doesn’t already exist
-# Create the folder /data/web_static/ if it doesn’t already exist
-# Create the folder /data/web_static/releases/ if it doesn’t already exist
-# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
-mkdir -p /data/web_static/shared/
-# Create the folder /data/web_static/shared/ if it doesn’t already exist
-echo '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>' > /data/web_static/releases/test/index.html
-# Create a fake HTML file /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-# Create a symbolic link /data/web_static/current linked to the...
-# .../data/web_static/releases/test/ folder. If the symbolic link already...
-# ...exists, it should be deleted and recreated every time the script is ran.
-chown -hR ubuntu:ubuntu /data/
-# Give ownership of the /data/ folder to the ubuntu user AND group (you can
-# assume this user and group exist). This should be recursive; everything
-# inside should be created/owned by this user/group.
-sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-# Update the Nginx configuration to serve the content of
-# /data/web_static/current/ to hbnb_static
-# (ex: https://mydomainname.tech/hbnb_static).
-# Don’t forget to restart Nginx after updating the configuration:
-service nginx restart
+# Install Nginx if not already installed
+if ! dpkg -l | grep -q nginx; then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
+
+# Define directories
+web_static_dir="/data/web_static"
+releases_dir="$web_static_dir/releases/test"
+current_dir="$web_static_dir/current"
+index_html="$releases_dir/index.html"
+
+# Create necessary directories if they don't exist
+sudo mkdir -p "$web_static_dir" "$releases_dir"
+
+# Create or update index.html file
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee "$index_html"
+
+# Create or update symbolic link (delete and recreate if it exists)
+if [ -L "$current_dir" ]; then
+    sudo rm -f "$current_dir"
+fi
+sudo ln -s "$releases_dir" "$current_dir"
+# Set ownership recursively
+sudo chown -R ubuntu:ubuntu "$web_static_dir"
+
+# Update Nginx configuration using alias
+nginx_config="/etc/nginx/sites-available/default"
+echo "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    location /hbnb_static/ {
+        alias $current_dir/;
+    }
+}" | sudo tee "$nginx_config"
+
+# Restart Nginx to apply changes
+sudo service nginx restart
+
+# Check and print the "msg" variable
+if [ "$msg" -eq 0 ]; then
+    echo "msg - [Expected] 0"
+else
+    echo "msg - [Got] $msg"
+fi
