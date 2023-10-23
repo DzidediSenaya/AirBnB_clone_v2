7600a4d403617ddd5012f68cb27def27b5da0f31
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-10-23 00:36:17 +0000

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application.
+"""Starts a Flask web application.
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
-    /hbnb: Displays 'HBNB'
-    /c/<text>: Displays 'C ', followed by the value of the text variable
-    (Replace underscore _ symbols with a space)
-    /python/(<text>): Displays 'Python ', followed by the value
-     of the text variable
-    (Replace underscore _ symbols with a space)
-    /number/<n>: Displays 'n is a number' only if n is an integer
-    /number_template/<n>: Displays an HTML page with "Number: n" inside
-     the H1 tag, but only if n is an integer
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: Displays 'n is a number' only if <n> is an integer.
+    /number_template/<n>: Displays an HTML page only if <n> is an integer.
 """
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -46,7 +43,7 @@ def c(text):
 def python(text="is cool"):
     """Displays 'Python' followed by the value of <text>
 
-    Replaces all underscores in <text> with slashes.
+    Replaces any underscores in <text> with slashes.
     """
     text = text.replace("_", " ")
     return "Python {}".format(text)
