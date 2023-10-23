580ad7c8af35fe90b91148cea3ef26ffa329ec50
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-10-23 00:35:16 +0000

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -16,9 +16,7 @@ Routes:
      the H1 tag, but only if n is an integer
 """
 
-from flask import Flask, render_template
-
-app = Flask(__name)
+app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
@@ -34,29 +32,37 @@ def hbnb():
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c_text(text):
-    """Displays 'C ', followed by the value of the text variable"""
-    return "C " + text.replace("_", " ")
+def c(text):
+    """Displays 'C' followed by the value of <text>
+
+    Replaces any underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-@app.route("/python/", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def python_text(text="is cool"):
-    """Displays 'Python ', followed by the value of the text variable"""
-    return "Python " + text.replace("_", " ")
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>
+
+    Replaces all underscores in <text> with slashes.
+    """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """Displays 'n is a number' only if n is an integer"""
-    return f"{n} is a number"
+    """Displays 'n is a number' only if <n> is an integer."""
+    return "{} is a number".format(n)
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    """Displays an HTML page with 'Number: n' inside the H1 tag, but only if n is an integer"""
-    return render_template('5-number.html', number=n)
+    """Displays an HTML page only if <n> is an integer."""
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
